362029d8d0df67e901f00e83b9b1fc6a2043c1af
Paul Huallpa <pantoni123@gmail.com>
2023-03-13 18:02:47 -0600

    Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
-#what
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :owned_offers, foreign_key: :owner_id, class_name: "Offers"
+  has_many :owner_bookings, through: :owned_offers, source: :bookings
+
+  has_many :client_bookings, foreign_key: :client_id, class_name: "Bookings"
+  has_many :booked_offers, through: :client_bookings, source: :offers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_214414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "total_price"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "offers", force: :cascade do |t|
     t.string "brand"
     t.string "model"
@@ -28,15 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_214414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_offers_on_user_id"
-  end
-
-
-  create_table "bookings", force: :cascade do |t|
-    t.date "start_date"
-    t.date "end_date"
-    t.float "total_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
