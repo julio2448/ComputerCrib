b630f0fabb1b613d711f888fa88b8075d6a3e9b3
Julio Vazquez <jv4271@gmail.com>
2023-03-16 16:22:19 -0600

    geo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_16_162507) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
